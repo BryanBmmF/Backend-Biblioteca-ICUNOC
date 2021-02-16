1ec52151b40d78c256c30e8da6fc9fbcaabd3442
Bryan Monzón <bryanmisael-monzonfuentes@cunoc.edu.gt>
2021-02-16 13:52:07 -0600

    Especificacion del plugin en el pom

--- src/main/java/com/icunoc/biblioteca/pruebas/Calculator.java
+++ src/main/java/com/icunoc/biblioteca/pruebas/Calculator.java
@@ -1,2 +1,16 @@
-package com.icunoc.biblioteca.pruebas;public class Calculator {
+package com.icunoc.biblioteca.pruebas;
+
+public class Calculator {
+    /**
+     * Metodo de prueba que suma una serie de numeros positivos
+     * */
+    public int sumPositiveValues(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            if(number>0){
+                sum += number;
+            }
+        }
+        return sum;
+    }
 }
