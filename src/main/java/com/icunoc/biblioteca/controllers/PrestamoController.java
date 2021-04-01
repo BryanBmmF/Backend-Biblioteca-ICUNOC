3d57a30665edd8edc81418eff1222712fb632995
richie77mendez <augustoricardo-mendezpum@cunoc.edu.gt>
2021-03-31 19:48:06 -0600

    Listado de prestamos y visualizacion de moras segun la fecha de inicio de un prestamo (max de 7 dias para un prestamo sin mora)

--- src/main/java/com/icunoc/biblioteca/controllers/PrestamoController.java
+++ src/main/java/com/icunoc/biblioteca/controllers/PrestamoController.java
@@ -9,13 +9,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping({"/prestamos"})
 public class PrestamoController {
-
+    private static final int DIAS_PRESTAMO=7;
+    private static final int DIAS_MOROSO_RESTART=0;
     @Autowired
     PrestamoService service;
+    //metodo para mandar una lista de libros al cliente
+    @GetMapping("/listaPrestamo/{estado}")
+    public ResponseEntity<List<Prestamo>> listarPrestamos(@PathVariable("estado") String estado){
+        List<Prestamo> list = service.list(estado);
+        for (int i=0; i<list.toArray().length; i++){
+            Calendar miFecha = Calendar.getInstance();
+            int milisecondsByDay = 86400000;
+
+            int dias = (int) ((miFecha.getTime().getTime()-list.get(i).getFechaInicio().getTime().getTime()) / milisecondsByDay);
+            if (dias>DIAS_PRESTAMO){
+                int diasAtrasado = dias-DIAS_PRESTAMO;
+                System.out.println(diasAtrasado);
+                list.get(i).setDiasMoroso(diasAtrasado);
+                list.get(i).setMora(true);
+            }else{
+                list.get(i).setDiasMoroso(DIAS_MOROSO_RESTART);
+                list.get(i).setMora(false);
+            }
+            service.save(list.get(i));
+        }
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
 
     @GetMapping(path = {"/{codigoReservacion}"})
     public Prestamo listarReservacion(@PathVariable("codigoReservacion") String codigo){
@@ -35,6 +62,7 @@ public class PrestamoController {
                 prestamoDto.getFechaReservacion(),
                 prestamoDto.getEstado(),
                 prestamoDto.getCodigoReservacion(),
+                prestamoDto.isMora(),
                 prestamoDto.getCodigoLibro()
         );
         service.save(nuevoPrestamo);
--- src/main/java/com/icunoc/biblioteca/services/PrestamoServiceImpl.java
+++ src/main/java/com/icunoc/biblioteca/services/PrestamoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.icunoc.biblioteca.services;
 
+import com.icunoc.biblioteca.models.Libro;
 import com.icunoc.biblioteca.models.Prestamo;
 import com.icunoc.biblioteca.repositories.PrestamoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,16 @@ public class PrestamoServiceImpl implements PrestamoService {
     public void save(Prestamo prestamo) {
         //guardamos el libro
         repository.save(prestamo);
+    }
+    @Override
+    public List<Prestamo> list(String estado) {
+        return repository.findByEstado(estado);
+    }
+
+    @Override
+    public Prestamo getOne(String id){
+
+        return repository.findByCodigoReservacion(id);
     }
 
     public void setRepository(PrestamoRepository prestamoRepository) {
